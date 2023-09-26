c9d87a4cdd7e61de376210c5ac50b580451e095d
jube <jube@itu.dk>
2023-09-26 10:46:13 +0200

    changes

--- README.md
+++ README.md
@@ -1,13 +1,36 @@
 # TCP-IP_SimulatorInGo
+
 Repo for the handin 2 in BSDISYS1KU in 2023, simulating TCP/IP in Golang to understand the workings and importance of the TCP protocol
 
 # Questions for the handin
+
 a) What are packages in your implementation? What data structure do you use to transmit data and meta-data?
+
+    In our implementation we have simulated a network with data exchanged between a client and server.
+
+    In our case the data and metadata are combined into strings and sent over channels between the server and client using the send and receive channels.
+
+    These strings contain the relevant data and metadata separated by spaces. For establishing connection that is SYN (synchronisation requests), ACK (acknowledgement) messages (this includes the server and client ISN). For the data sent after the connection has been established this means a sequence number as well as the ‘actual’ data
 
 b) Does your implementation use threads or processes? Why is it not realistic to use threads?
 
+    Using threads does not make for a realistic simulation of a web connection since they lack the uncertainty and delay which exists in the real web. This means that it is not necessary to take into account packages/messages that get lost or arrive out of order in a simulation using threads since threads operate in a predictable way. Web connections involve dynamic factors such as network latency, delays and variable and unpredictable response times from servers.
+
 c) In case the network changes the order in which messages are delivered, how would you handle message re-ordering?
+
+    Because messages may be delivered out of order (or not at all) in a real web connection, it is necessary to think about how to deal with these cases.
+    Message acknowledgements and sequence numbers: We use message acknowledgements and sequence numbers to keep track of which messages sent by the client have been received by the server. This gives us a way to consider reordering messages out of order, become aware of lost messages.
+    We have (attempted to) implement a timeout logic which will make the client aware if the server has not recieved a message, so that this can be dealt with. If a acknowledgement of a message with a specific sequence number is expected but doesn't arrive within a certain time frame, we should consider the message lost and we could request a transmission of the same message again.
+    In the current implementation we check that the sequence number of the received acknowledgement matches the expected sequence number. We could implement functionality to order the data if it is out of order based on the sequence numbers by temporarily storing it until the missing messages arrive. If the sequence number is too far from the expected we could consider it lost and request it be sent again.
 
 d) In case messages can be delayed or lost, how does your implementation handle message loss?
 
+    The current implementation does not handle this case …
+
 e) Why is the 3-way handshake important?
+
+    The 3-way handshake is an important part of establishing reliable and stable connection between two parties over a server or network. Between a client and a server, the 3-way handshake can ensure that both the client and server are aware of and expecting to communicate. The proces involved:
+        1. The client sends SYN (synchronisation) request to the server to initiate the connection
+        2. The server acknowledges the SYN request and sends back a SYN-ACK (synchronize-acknowledgment) message/packet
+        3. The client sends back an ACK message/packet to acknowledge the servers response, and both parties can start exchanging data.
+    By exchanging these initial packets while establishing the connection, both the client and server can confirm that the other party is reachable and responsive. If any of these packets are lost or not received, or not responded to in the expected way the connection is not established, and the sender can retry the handshake. The 3-way handshake is also used to establish the sequence numbers which help guarantee that the packets during the subsequent communication arrive in the correct order.
--- ip_simulator.go
+++ ip_simulator.go
@@ -2,87 +2,162 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"strconv"
 	"strings"
 	"time"
 )
 
 func client(send chan string, recieve chan string) {
-	fmt.Println("SERVER MADE")
-	//send sequence
-	var seq = 1234
-	send <- strconv.Itoa(seq)
 
-	//recieve acknowledgement and send acknowledgement + data
+	fmt.Println("CLIENT MADE")
+	var connected = false
 
-	var serverresponse = <-recieve
-	var split = strings.Split(serverresponse, " ")
+	var isn = generateISNForClient()
 
-	var recievedseq int
-	var err error
-	recievedseq, err = strconv.Atoi(split[0])
-	if seq != (recievedseq - 1) {
-		fmt.Print("Oh dear! The recieved sequence is NOT the same!")
-		return
-	}
+	//wait to make sure the server is ready to recieve messages before sending
+	time.Sleep(time.Millisecond * time.Duration(1000))
 
-	var ack int
-	ack, err = strconv.Atoi(split[1])
-	if err != nil {
-		fmt.Print("Oh dear! Something went wrong when recieving acknowledgement from the server")
-	}
-	ack = ack + 1
-	//data send
-	for {
-		var data string
-		data = "someting"
-		send <- strconv.Itoa(ack) + " " + strconv.Itoa(seq) + " " + data
+	//first establish connection to server using 3-way handshake
+	if !connected {
+		time.Sleep(time.Millisecond * time.Duration(3000))
+		fmt.Println("CLIENT: Trying to connect to server...")
 
-		serverresponse = <-recieve
-		split = strings.Split(serverresponse, " ")
-		recievedseq, err = strconv.Atoi(split[0])
-		if seq != (recievedseq - 1) {
-			fmt.Print("Oh dear! The recieved sequence is NOT the same!")
-			return
+		//1: Send synchronization request
+		var message = "SYN " + strconv.Itoa(isn)
+		send <- message
+		isn += 1
+
+		//2: recieve synchronization acknowledgement from server
+		var serverresponse = <-recieve
+		var split = strings.Split(serverresponse, " ")
+
+		// 3: Send back acknowledgment of server response with ACK message.
+		if split[0] == "SYN" && split[1] == "ACK" && split[2] == strconv.Itoa(isn) {
+			serverISN, err := strconv.Atoi(split[3])
+			if err == nil {
+				message = "ACK " + strconv.Itoa(serverISN+1) + " " + strconv.Itoa(isn)
+				send <- message
+				connected = true
+				fmt.Println("CLIENT: Connected to server")
+			}
 		}
-		ack, err = strconv.Atoi(split[1])
-		if err != nil {
-			fmt.Print("Oh dear! Something went wrong when recieving acknowledgement from the server")
+	}
+	//After connection is established, we send data!
+	var sequenceNumber = isn
+	var listeningForAck = true
+	for connected {
+		//generate sequence number string + data (random number between 0 and 100) as string and send it
+		stringSequenceNumber := strconv.Itoa(sequenceNumber)
+		data := strconv.Itoa(rand.Intn(100))
+		//increment sequence number
+		sequenceNumber++
+
+		//create and send message
+		var message = "SEQUENCE NUMBER: " + stringSequenceNumber + " DATA: " + data
+		send <- message
+
+		//wait for ACK response from server for sent message (with same sequence number) or timeout
+
+		listeningForAck = true
+		for listeningForAck {
+			select {
+			case ack := <-recieve:
+				if strings.Contains(ack, "SEQUENCE NUMBER: "+stringSequenceNumber) {
+					// Received the expected ACK from server
+					fmt.Println("CLIENT: RECIEVED ACKNOWLEDGEMENT FROM SERVER FOR RECIEVED MESSAGE WITH SEQUENCE NUMBER: ", stringSequenceNumber, ack)
+					listeningForAck = false
+					break
+				} else {
+					//handle out of order/missing ACKs somehow
+					listeningForAck = false
+					break
+				}
+			case <-time.After(10000 * time.Millisecond):
+				// Timeout occurred.
+				fmt.Println("CLIENT: TIMEOUT OCCURRED WAITING FOR ACKNOWLEDGEMENT FROM SERVER FOR MESSAGE WITH SEQUENCE NUMBER", stringSequenceNumber)
+				listeningForAck = false
+				break
+			}
 		}
-		ack = ack + 1
-		fmt.Println(ack)
 	}
 }
 
 func server(send chan string, recieve chan string) {
 	fmt.Println("SERVER MADE")
-	//recieve sequence and send acknowledgement
-	var ack int
+
+	var isn = generateISNForServer()
 	var err error
-	var seq = 4321
-	ack, err = strconv.Atoi(<-recieve)
-	if err != nil {
-		fmt.Print("OH NO! The sequence was not recieved correctly by the server")
-	}
-	ack = ack + 1
-	send <- strconv.Itoa(ack) + " " + strconv.Itoa(seq)
 
-	for {
-		time.Sleep(time.Millisecond * time.Duration(1000))
-		var clientresponse = <-recieve
-		var split = strings.Split(clientresponse, " ")
-		var recievedseq int
-		recievedseq, err = strconv.Atoi(split[0])
-		if seq != (recievedseq - 1) {
-			fmt.Print("Oh dear! The recieved sequence is NOT the same!")
-			return
+	var connected = false
+
+	//wait for connection with client ...
+	for connected == false {
+		time.Sleep(time.Millisecond * time.Duration(3000))
+		fmt.Println("SERVER: Waiting for client to connect...")
+
+		//Recieve synchronization request
+		var syncReq = <-recieve
+		var split = strings.Split(syncReq, " ")
+
+		// client tries to connect to server using SYNchronization
+		if split[0] == "SYN" {
+			var clientISN, err = strconv.Atoi(split[1])
+			if err == nil {
+				//acknowledge synchronization attempt:
+				send <- "SYN ACK " + strconv.Itoa(clientISN+1) + " " + strconv.Itoa(isn)
+				isn += 1
+			}
 		}
-		ack, err = strconv.Atoi(split[1])
-		ack++
-		fmt.Println(split[1], split[2])
-		send <- strconv.Itoa(ack) + " " + strconv.Itoa(seq)
+
+		var ackReq = <-recieve
+		split = strings.Split(ackReq, " ")
+
+		// We validate ack message from client. If everything matches, then the 3-way handshake has been a success.
+		//A connection is established!
+		if split[0] == "ACK" {
+			//clientISN, err := strconv.Atoi(split[2])
+			//fmt.Println("CLIENT ISN:", clientISN)
+			//serverISN, err := strconv.Atoi(split[1])
+			//fmt.Println("SERVER ISN:", serverISN, isn)
+
+			if err == nil && split[1] == strconv.Itoa(isn) {
+				connected = true
+				fmt.Println("SERVER: CLIENT connected!")
+			} else {
+				fmt.Print("FAILURE TO CONNECT!")
+			}
+		}
 	}
 
+	//We simulate data reception and response
+	//wait before beginning to send data...
+	time.Sleep(time.Millisecond * time.Duration(3000))
+	for connected {
+
+		//we add a delay before sending responce
+		time.Sleep(time.Millisecond * time.Duration(1000))
+
+		//recieve request
+		var request = <-recieve
+		fmt.Println("SERVER: Recieved request from client:", request)
+
+		//Generate response and send back to client
+		var response = "SERVER: Response on request: " + request
+		send <- response
+
+	}
+}
+
+// To ensure that a unique sequence number is generated for both client and server we use
+// seperate functions to generate ISN for client and server.
+func generateISNForClient() int {
+	rand.Seed(time.Now().UnixNano())
+	return rand.Intn(1000)
+}
+func generateISNForServer() int {
+	rand.Seed(time.Now().UnixNano())
+	return rand.Intn(1000)
 }
 
 func main() {
